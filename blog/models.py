77c8313e830634c1279190327c0062ccabda11d1
linkghm <link2619@126.com>
2019-05-11 16:28:52 +0800

    create database structure model and create database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class BlogArticles(models.Model):
+    # 字段title属性charfield
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    # 规定blog article的显示顺序
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
